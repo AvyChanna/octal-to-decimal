0ba6ddf4a868d6981b85e6b5b67539e078d0de77
AvyChanna <30595749+AvyChanna@users.noreply.github.com>
2019-01-19 23:50:16 +0530

    Major BugFix
    - ".1" now does not give exception error
    - Negative Sign is now added
    - "123." is now recognized as invalid output
    - "10.00000" now outputs "8" (as expected instead of "8.00000")
    - Started coding visuals

--- OctalToDecimal/ValidationFunctionModule.vb
+++ OctalToDecimal/ValidationFunctionModule.vb
@@ -5,27 +5,35 @@
             MessageBox.Show("Please insert a number first !", "No Input", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             Return False
         End If
-        Dim NotFirst, HasDot, HasSign As Boolean
-        NotFirst = False
+		Dim NotFirst, HasDot, HasSign, DigitAfterDot As Boolean
+		NotFirst = False
         HasDot = False
-        HasSign = False
-        For Each c As Char In str
-            If Asc(c) >= Asc("0") AndAlso Asc(c) <= Asc("7") Then
-            ElseIf Asc(c) = Asc("-") AndAlso NotFirst = False AndAlso HasSign = False Then
-                HasSign = True
-            ElseIf Asc(c) = Asc("+") AndAlso NotFirst = False AndAlso HasSign = False Then
-                HasSign = True
-            ElseIf Asc(c) = Asc(".") AndAlso HasDot = False Then
-                HasDot = True
-            Else
-                MessageBox.Show("Please check your number !", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-                Return False
-            End If
-            NotFirst = True
-        Next
+		HasSign = False
+		DigitAfterDot = False
+		For Each c As Char In str
+			If Asc(c) >= Asc("0") AndAlso Asc(c) <= Asc("7") Then
+				If HasDot = True Then
+					DigitAfterDot = True
+				End If
+			ElseIf Asc(c) = Asc("-") AndAlso NotFirst = False AndAlso HasSign = False Then
+				HasSign = True
+			ElseIf Asc(c) = Asc("+") AndAlso NotFirst = False AndAlso HasSign = False Then
+				HasSign = True
+			ElseIf Asc(c) = Asc(".") AndAlso HasDot = False Then
+				HasDot = True
+			Else
+				MessageBox.Show("Please check your number !", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+				Return False
+			End If
+			NotFirst = True
+		Next
+		If DigitAfterDot = False AndAlso HasDot = True Then
+			MessageBox.Show("Please check your number !", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+			Return False
+		End If
 
-        'Input correct as all cases done and accounted for
-        'Input still unable to process , in inputs 
-        Return True
+		'Input correct as all cases done and accounted for
+		'Input still unable to process , in inputs 
+		Return True
     End Function
 End Module
--- OctalToDecimal/convertor.vb
+++ OctalToDecimal/convertor.vb
@@ -2,22 +2,33 @@
 
     Function convertDec(ByVal str As String) As String
 
-        'split into 2 halves before decimal and after decimal
-        Dim befDec, aftDec As String
-        befDec = ""
+		'split into 2 halves before decimal and after decimal
+		Dim befDec, aftDec, aftDec2, befDec2 As String
+		befDec = ""
+        befDec2 = ""
         aftDec = ""
+        aftDec2 = ""
 
         Dim foundDot As Boolean = False
-        For Each c As Char In str
-            If c = "." Then
-                foundDot = True
-            ElseIf foundDot Then
-                aftDec += c
-            Else
-                befDec += c
-            End If
-        Next
-
+		For Each c As Char In str
+			If c = "." Then
+				foundDot = True
+			ElseIf c = "+" Or c = "-" Then
+				Continue For
+			ElseIf foundDot Then
+				aftDec += c
+			Else
+				befDec += c
+			End If
+		Next
+		If aftDec = "" Then
+			aftDec = "0"
+		End If
+		If befDec = "" Then
+			befDec = "0"
+		End If
+		aftDec2 = aftDec
+        befDec2 = befDec
         '*********************************initialisations*********************************************
 
         Dim resBefDec, multiplicant, remainder As System.Numerics.BigInteger
@@ -49,14 +60,17 @@
             resAftDec = supportFunc(resAftDec, count, aftDec(0))
         End If
 
-        '***********************************pretty return string*****************************************
-
-        If System.Numerics.BigInteger.Parse(resAftDec) > 0 Then
-            Return resBefDec.ToString + "." + resAftDec
-        Else
-            Return resBefDec.ToString
-        End If
-
+		'***********************************pretty return string*****************************************
+		Dim result As String = ""
+		If str.StartsWith("-") Then
+			result = "-"
+		End If
+		result = result + resBefDec.ToString
+		If System.Numerics.BigInteger.Parse(resAftDec) > 0 Then
+			result = result + "." + resAftDec.TrimEnd(New Char() {"0"c})
+		End If
+		'writeExplanation(aftDec2,befDec2,result)
+		Return result
     End Function
 
     Function supportFunc(ByVal resAftDec As String, ByRef count As Integer, ByVal c As Char) As String
@@ -71,5 +85,46 @@
         count += 1
         Return (curr * 125).ToString
     End Function
+	'Function writeExplanation(ByVal integral As String, ByVal fractional As String, ByVal result As String) As String
+	'	Dim IntegralSize As Integer = Len(integral)
+	'	Dim FractionalSize As Integer = Len(fractional)
+	'	Dim RtfTemp As String = "{\rtf1\ansi\ansicpg1252\deff0\nouicompat\deflang1033{\fonttbl{\f0\fnil\fcharset0 Courier New;}}{\colortbl ;\red155\green0\blue211;\red128\green64\blue0;\red155\green187\blue89;\red0\green176\blue80;\red0\green77\blue187;\red255\green0\blue0;}\viewkind4\uc1\pard\sl240\slmult1\cf1\f0\fs20\lang9 "
+	'	' Line 1
+	'	For Each c As Char In integral
+	'		RtfTemp = RtfTemp + c + "\tab "
+	'	Next
 
+	'	If System.Numerics.BigInteger.Parse(fractional) > 0 Then
+	'		RtfTemp = RtfTemp + "\cf0 .\cf2"
+	'		For Each c As Char In fractional
+	'			RtfTemp = RtfTemp + "\tab " + c
+	'		Next
+	'	End If
+	'	RtfTemp = RtfTemp + "\par\cf3"
+	'	' Line 2
+	'	For Each c As Char In integral
+	'		RtfTemp = RtfTemp + "X\tab "
+	'	Next
+
+	'	If System.Numerics.BigInteger.Parse(fractional) > 0 Then
+	'		RtfTemp = RtfTemp + "\cf0 .\cf2"
+	'		For Each c As Char In fractional
+	'			RtfTemp = RtfTemp + "\tab X"
+	'		Next
+	'	End If
+	'	RtfTemp = RtfTemp + "\par\cf2 "
+	'	' Line 3
+	'	For c As Integer = IntegralSize - 1 To 0
+	'		RtfTemp = RtfTemp + "8\super " + c.ToString + "\nosupersub\tab "
+	'	Next
+
+	'	If System.Numerics.BigInteger.Parse(fractional) > 0 Then
+	'		RtfTemp = RtfTemp + "\cf0 .\cf2"
+	'		For c As Integer = 1 To Len(fractional)
+	'			RtfTemp = RtfTemp + "\tab 8\super -" + c.ToString + "\nosupersub"
+	'		Next
+	'	End If
+	'	RtfTemp = RtfTemp + "\par\par\}"
+	'	Return "0"
+	'End Function
 End Module
